329d5f572fa62ab08d7db09745cda966c3f7e692
Luke Bennett <hello@lukebennett.com.au>
2022-03-20 17:59:31 +1100

    Use Link in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,10 +1,12 @@
+import { Link } from "remix";
+
 export function Header() {
   return (
     <header className="mx-auto w-full max-w-7xl sm:px-6 lg:px-8">
       <div className="border-b border-gray-200 px-4 sm:px-0">
         <div className="flex h-16 items-center justify-between">
           <div className="flex flex-1">
-            <a href="/">
+            <Link to="/">
               <span className="sr-only">Digital Design Assets</span>
               <svg
                 className="h-8 w-8 text-purple-600"
@@ -19,7 +21,7 @@ export function Header() {
                   fill="currentColor"
                 />
               </svg>
-            </a>
+            </Link>
           </div>
           <div className="flex flex-1 items-center justify-end">
             <a href="#" className="p-2 text-gray-400 hover:text-gray-500">
